309bb536fc6ce7213bdfc3b0b2411fed13f21dae
bangjaeyoung <qkdwo4613@gmail.com>
2022-06-16 01:01:52 +0900

    third commit

--- BinaryTest.java
+++ BinaryTest.java
@@ -1,6 +1,7 @@
 public class BinaryTest {
 
     public static void main(String[] args) {
+
         int num = 10;
         int bNum = 0B1010;
         int oNum = 012;
@@ -10,5 +11,6 @@ public class BinaryTest {
         System.out.println(bNum);
         System.out.println(oNum);
         System.out.println(xNum);
+        
     }
 }
