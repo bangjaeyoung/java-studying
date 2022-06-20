407eb3a6826ad4ccd0194222e1bbf86475c9ca3d
bangjaeyoung <qkdwo4613@gmail.com>
2022-06-21 00:00:11 +0900

    second commit

--- ch20/TimesTable2.java
+++ ch20/TimesTable2.java
@@ -9,14 +9,19 @@ public class TimesTable2 {
         System.out.println("구구단");
 
         while ( i < 10 ) {
+
             int j = 1;
+        
             while ( j < 10 ) {
 
                 System.out.println(i + " x " + j + " = " + i * j);
                 j++;
 
             }
+
             i++;
+            System.out.println();
+        
         }
     }
 }
