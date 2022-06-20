d4f73dfd30a120042a147d07047563e06830edbb
bangjaeyoung <qkdwo4613@gmail.com>
2022-06-20 19:17:34 +0900

    second commit

---
+++ ch17/While.java
@@ -0,0 +1,18 @@
+package ch17;
+
+public class While {
+
+    public static void main(String[] args) {
+
+        int result = 0;
+        int num = 0;
+
+        while (num < 10) {
+            ++num;
+            result += num;
+        }
+        System.out.println(result);
+        System.out.println(num);
+    }
+    
+}
