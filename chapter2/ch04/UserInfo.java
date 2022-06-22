474bce37a3a99c832a3ae284874df61e109faa8c
bangjaeyoung <qkdwo4613@gmail.com>
2022-06-22 17:49:44 +0900

    third commit

--- chapter2/ch04/UserInfo.java
+++ chapter2/ch04/UserInfo.java
@@ -13,15 +13,9 @@ public void setUserName(String name) {
 
 }
 
-public void setUserID(String id) {
-    userID = id;
-
-}
-
 public void showUserInfo() {
 
     System.out.println("사용자 이름 : " + userName + "\n사용자 ID : " + userID + "\n사용자 Pass : " + userPassword + "\n사용자 주소 : " + userAddress + "\n사용자 전화번호 : " + phoneNumber);
 
-}
-
+    }
 }
--- chapter2/ch04/UserInfoTest.java
+++ chapter2/ch04/UserInfoTest.java
@@ -6,7 +6,7 @@ public class UserInfoTest {
 
         UserInfo user1 = new UserInfo();
         user1.setUserName("방재영");
-        user1.setUserID("Bang");
+        user1.userID = "Bang";
         user1.userPassword = "123";
         user1.userAddress = "인천시";
         user1.phoneNumber = "010-4106-3333";
@@ -15,13 +15,12 @@ public class UserInfoTest {
 
         UserInfo user2 = new UserInfo();
         user2.setUserName("홍길동");
-        user2.setUserID("Hong");
+        user2.userID = "Hong";
         user2.userPassword = "456";
         user2.userAddress = "서울시";
         user2.phoneNumber = "010-3140-3563";
 
         user2.showUserInfo();
         
-    }
-    
+    }   
 }
