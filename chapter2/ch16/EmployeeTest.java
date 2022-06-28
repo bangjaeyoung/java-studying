3cfce3c5f5719df93adf77784a72f615003da6f6
bangjaeyoung <qkdwo4613@gmail.com>
2022-06-28 16:29:03 +0900

    third commit

--- chapter2/ch16/EmployeeTest.java
+++ chapter2/ch16/EmployeeTest.java
@@ -17,8 +17,9 @@ public class EmployeeTest {
         System.out.println(Employee.serialNum);              // 결과값 1001
         // 두 개의 인스턴스가 하나의 메모리를 공유하는 걸 알 수 있다. (static 변수)
 
-        //지역 변수 employeeLee, employeeKim은 스택 메모리에 저장
-        //이것들로 인해서 참조되는 인스턴스(employeeLee.~, employeeKim.~)는 힙 메모리(동적 메모리)에 저장, 다 쓰면 소멸
-        //employeeLee, employeeKim이 참조할 수 있는 static 변수는 공유 메모리에 저장
+        //지역 변수 employeeLee, employeeKim은 스택 메모리에 저장된다.
+        //이것들로 인해서 참조되는 인스턴스(employeeLee.~, employeeKim.~)는 힙 메모리(동적 메모리)에 저장, 다 쓰면 소멸된다.
+        //employeeLee, employeeKim이 참조할 수 있는 static 변수는 공유 메모리에 저장된다.
+        //static 변수는 클래스 이름으로 참조해서 쓴다.
     }
 }
